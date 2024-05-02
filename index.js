2c61ef6b431c2fea567d6cf41fe17def0ae5cbf1
ilona <gogiashvili.ilona@gmail.com>
2024-05-02 16:21:37 -0400

    commit tree a8a74235cc59c697d99504183573421263cc12a7

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  let t = tutorials;
+
+  let cent = t.map((tutorial) => {
+    let words = tutorial.split(" ");
+    // console.log(result);
+    let result = words.map(
+      (item) => item.charAt(0).toUpperCase() + item.slice(1)
+    );
+
+    return result.join(" ");
+  });
+  return cent;
+  // if (tutorial.charAt(0)) {
+  //   let word = tutorial.charAt(0).toUpperCase();
+  //   console.log(word);
+  // } else {
+  //   return null;
+};
+console.log(titleCased());
